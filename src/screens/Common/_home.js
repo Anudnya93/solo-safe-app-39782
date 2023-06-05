6e5c4c6ea588142142f8c316002a57174b5deeae
Anudnya93 <anudnya.durgule@crowdbotics.com>
2023-06-05 18:16:56 +0530

    initial ios lock unlock

--- src/screens/Common/_home.js
+++ src/screens/Common/_home.js
@@ -1,8 +1,64 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useEffect } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  NativeEventEmitter,
+  NativeModules
+} from 'react-native'
 import Icon from '../../components/Icon'
 
+let lockDetectionSuscription
+
 const Home = () => {
+  const { LockDetection } = NativeModules
+
+  console.log('rndetection', LockDetection)
+
+  useEffect(() => {
+    LockDetection.registerforDeviceLockNotif() // Register the library to listen the events for Darwin notifications
+    const LockDetectionEmitter = new NativeEventEmitter(LockDetection) // Create instance of EventEmitter
+    lockDetectionSuscription = LockDetectionEmitter.addListener(
+      // and add the listener
+      'LockStatusChange',
+      newStatus => {
+        console.log('new status', newStatus) // Do whatever you need with the information
+      }
+    )
+
+    // android another implementation better working
+
+    // const eventEmitter = new NativeEventEmitter(NativeModules.PhoneLocked)
+
+    // eventEmitter.addListener('EventReminder', res => {
+    //   // listen here for screen on or off action
+    //   console.log(res.action) // ACTION_USER_PRESENT || ACTION_SCREEN_OFF || ACTION_SCREEN_ON
+    // })
+
+    // android implementation 2 end
+
+    // android implementation working
+
+    // const RNLockDetection = NativeModules.RNLockDetection
+    // console.log('RNLockDetection', RNLockDetection)
+
+    // interval = setInterval(() => {
+    //   RNLockDetection.isLock().then(callback => {
+    //     //calback = true is mode sleep
+    //     if (callback) {
+    //       console.log('Mode Sleep')
+    //     } else {
+    //       console.log('Active screen')
+    //     }
+    //   })
+    // }, 1000)
+
+    // android implementation end
+    return () => {
+      // clearInterval(interval)
+      lockDetectionSuscription.remove()
+    }
+  }, [])
   return (
     <View style={styles.container}>
       <Text>Home</Text>
